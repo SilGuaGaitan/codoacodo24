10385bd082bd5bec1d305eeb6916c8c8a5d22ba2
SilGuaGaitan <dragonrosadito@gmail.com>
2024-05-19 20:43:40 -0300

    formok

--- formulario.js
+++ formulario.js
@@ -1,14 +1,11 @@
+
 const button = document.querySelector("#button");
 const step1 = document.querySelector("#step-1");
 const step2 = document.querySelector("#step-2");
 const summary = document.querySelector("#summary");
-const btn_print= document.getElementById("btn_print");
-  
-//btn_print.onclick=()=>{window.print};//
-btn_print.onclick=()=>alert("Se manda a imprimir");
-const button_c= document.getElementById("button_c");
+const button_b =document.querySelector("#button_b");
+const button_c= document.getElementById('button_c');
 
-button_c.onclick=()=>{window.location.reload};
 //agregar evento onclick al botón siguiente
 button.addEventListener("click", function() {
   // recopilar datos del primer paso
@@ -28,5 +25,10 @@ button.addEventListener("click", function() {
   // ocultar el primer paso y mostrar el segundo paso
   step1.style.display = "none";
   step2.style.display = "block";
-  
 });
+
+const btn_print = document.getElementById('btn_print')
+btn_print.onclick = () => { window.print() }
+
+const buttonc = document.getElementById('button_c')
+buttonc.onclick = () => { window.location.reload() }
